b746a6eb95a6c28440a49c9ec4a09f21cd5706d8
AYESHA-AJ <ayeshaaj25@gmail.com>
2024-05-06 19:54:34 +0500

    project completed

--- 06currencyConvertor/src/components/InputBox.jsx
+++ 06currencyConvertor/src/components/InputBox.jsx
@@ -18,11 +18,11 @@ function InputBox({
     return (
         <div className={`bg-white p-3 rounded-lg text-sm flex ${className}`}>
             <div className="w-1/2">
-                <label htmlFor={} className="text-black/40 mb-2 inline-block">
+                <label htmlFor={amountInputId} className="text-black/40 mb-2 inline-block">
                     {label}
                 </label>
                 <input
-                    id={}
+                    id={amountInputId}
                     className="outline-none w-full bg-transparent py-1.5"
                     type="number"
                     placeholder="Amount"
--- 06currencyConvertor/src/hooks/useCurrencyInfo.js
+++ 06currencyConvertor/src/hooks/useCurrencyInfo.js
@@ -1,1 +1,18 @@
-import { useEffect,useState } from "react";
+import { useEffect, useState } from "react";
+
+
+//this hook will return data 
+function useCurrencyInfo(currency) {
+    const [data,setData]=useState({})
+    //we want that when this hook load or is used then this api is called so we use useEffect it automaticaly calls fetch
+    useEffect(() => {
+        fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`)
+            .then((res) => res.json())
+            .then((res) => setData(res[currency]))
+        console.log(data);
+    }, [currency])
+    console.log(data);
+    return data
+}
+//returning method
+export default useCurrencyInfo;
